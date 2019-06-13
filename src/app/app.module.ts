ceb6f1cde3e8059818626af184441af661c410f8
Ilona <ilonabarashkina@gmail.com>
2019-06-12 23:32:33 -0400

    Updated the nav bar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
-
+import { RouterModule, Routes } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AboutComponent } from './about/about.component';
 import { SearchComponent } from './search/search.component';
-import { LeaderboardComponent } from './leaderboard/leaderboard.component';
 import { FavoritesComponent } from './favorites/favorites.component';
 import { HomeComponent } from './home/home.component';
 import { NavComponent } from './nav/nav.component';
@@ -15,12 +14,19 @@ import { FooterComponent } from './footer/footer.component';
 
 import { DataService } from './data.service';
 
+
+const appRoutes: Routes = [
+  {path: 'home/:page', component: HomeComponent, pathMatch: 'full' },
+  {path: 'favorites', component: FavoritesComponent}, 
+  {path: '', redirectTo: 'home/1', pathMatch: 'full' },
+  {path: '**', redirectTo: 'home/1'}
+];
+
 @NgModule({
   declarations: [
     AppComponent,
     AboutComponent,
     SearchComponent,
-    LeaderboardComponent,
     FavoritesComponent,
     HomeComponent,
     NavComponent,
@@ -30,7 +36,8 @@ import { DataService } from './data.service';
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [DataService],
   bootstrap: [AppComponent]
