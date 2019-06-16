0a1b1aef59bfb886ea7b92e5837b05a6e6d77f70
BreaJanea <breajanea@gmail.com>
2019-06-16 14:25:41 -0400

    minor changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-
 import { AgmCoreModule } from '@agm/core';
 import { RouterModule, Routes } from '@angular/router';
-
-
 import { HttpClientModule } from '@angular/common/http';
 
 
