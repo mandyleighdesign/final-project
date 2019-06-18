867829d34a65626c8043e7717ee1614f0969b00c
Mandy <mandyleighdesign@gmail.com>
2019-06-18 13:26:52 -0400

    THE API WORKS OMG

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -8,8 +8,12 @@ import {DataService} from '../data.service';
 })
 
 export class HomeComponent {
-  city: String
-  selectedMaterial: String
+  city: String;
+  selectedMaterial: String;
+  locationName: any[];
+  locations = [];
+
+
   
   constructor(private dataService: DataService) {}
 
@@ -28,8 +32,12 @@ export class HomeComponent {
       const latLng = locationData['results'][0].geometry.location;
       console.log(latLng);
       this.dataService.getUsers(latLng).subscribe((res: any) => {
-        console.log(res.result);
+        this.locations = res.result;
+        console.log(this.locations);
       })
+      
     })
   }
+
+
 }
